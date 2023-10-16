90b80c86cff8abc0c0d622fcec77bf9626ad7939
uday21bt <akonsassylum21@gmail.com>
2023-10-17 01:59:13 +0530

    comit

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -16,15 +16,19 @@ const Row = ({title,fetchURL}) => {
     <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
     <div className='relative flex items-center'>
         <div id={'slider'}>
-            {movies.map((item,id)=>{
-                <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block' >
-                    <img src={`https://image.tmdb.org/t/p/original  /${item?.backdrop_path}`}alt={item?.title} />
-                </div>
-            })}
+            {movies.map((item,id)=>(
+ <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2' >
+          <img src={`https://image.tmdb.org/t/p/original/${item?.backdrop_path}`}alt={item?.title} />
+              </div>
+            ))}
         </div>
-    </div>
+    </div>                  
     </>
   )
 }
 
 export default Row
+
+// {movies.map((item,id)=>{
+//    
+// })}
